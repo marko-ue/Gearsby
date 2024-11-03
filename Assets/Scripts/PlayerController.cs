d3a166eef65572d71bcc98c00a61466e509ef6b0
Marko Petric <markop20042004@gmail.com>
2024-11-03 23:54:21 +0100

    Add PlayerHealth.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
@@ -5,9 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     private int pickupRange = 100;
 
+    [Header("Stamina Stats")]
+    [SerializeField] float stamina = 100f;
+    [SerializeField] float staminaDeduct = 2f;
+    [SerializeField] float staminaReturn = 3f;
+    [SerializeField] float maxStamina = 100f;
+    [SerializeField] float minStamina = 0f;
+    [SerializeField] float returnDelay = 1f;
+    [SerializeField] float returnTimer = 0f;
+
     [Header("Collectible Amount")]
     public int cogwheelCount = 0;
+    StarterAssetsInputs starterAssetsInputs;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        starterAssetsInputs = GetComponentInChildren<StarterAssetsInputs>();
+    }
     void Start()
     {
         
@@ -17,6 +32,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PickupRay();
+        Stamina();
     }
 
     private void PickupRay()
@@ -31,7 +47,32 @@ public class PlayerController : MonoBehaviour
             {
                 cogwheelCount++;
                 Debug.Log(cogwheelCount);
+                Destroy(hit.collider.gameObject);
             }
         }
+    }
+    void Stamina()
+    {
+        bool isMoving = starterAssetsInputs.move.magnitude > 0f; //Checks if the player is moving
+        // Deduct stamina when sprinting
+        if (starterAssetsInputs.sprint && isMoving)
+        {
+            stamina -= staminaDeduct * Time.deltaTime;
+            stamina = Mathf.Max(stamina, minStamina);
+            Debug.Log(stamina);
+            returnTimer = 0; // Reset the return timer when sprinting
+        }
+        else
+        {
+            returnTimer += Time.deltaTime; //return timer when not sprinting
+            if (returnTimer >= returnDelay)
+            {
+                // Return stamina only after delay
+                stamina += staminaReturn * Time.deltaTime;
+                stamina = Mathf.Min(stamina, maxStamina);
+                Debug.Log(stamina);
+            }
+        }
+        
     }
 }
