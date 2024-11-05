d200c58a06b91adf648da106282766d00a8ea9e6
Rxamz <cwithdeathwish@gmail.com>
2024-11-05 21:19:22 +0100

    Basic climbing done
    Did basic climbing onto the boxes crouching tmrw

---
+++ Assets/Scripts/Climbing.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Vaulting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int Climbable;
+    public Camera cam;
+    private float playerHeight = 2f;
+    private float playerRadius = 0.5f;
+    void Start()
+    {
+        Climbable = LayerMask.NameToLayer("Climbable");
+        Climbable = ~Climbable;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vault();
+    }
+    private void Vault()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out var firstHit, 1f, Climbable))
+            {
+                print("vaultable in front");
+                if (Physics.Raycast(firstHit.point + (cam.transform.forward * playerRadius) + (Vector3.up * 0.6f * playerHeight), Vector3.down, out var secondHit, playerHeight))
+                {
+                    print("found place to land");
+                    StartCoroutine(LerpVault(secondHit.point, 0.5f));
+                }
+            }
+        }
+
+    }
+    IEnumerator LerpVault(Vector3 targetPosition, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+    }
+}
